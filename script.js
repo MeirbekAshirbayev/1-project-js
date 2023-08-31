1197ef7f79a4bc177598b795fe68047fef0207ca
Miko <meirbekusenbek@gmail.com>
2023-08-31 11:10:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', ()=> {
+        removeAllClass()
+        panel.classList.add('active');
+    })
+})
+
+function removeAllClass() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
